ca925d219a914892faee704e1da931ac7dee2516
Clement <shawi.de.marmo@hotmail.fr>
2018-12-26 14:47:22 +0100

    ajout log4j

--- core/src/main/java/com/tatol/service/CalculatriceServiceImpl.java
+++ core/src/main/java/com/tatol/service/CalculatriceServiceImpl.java
@@ -1,28 +1,41 @@
 package com.tatol.service;
 
+import org.apache.log4j.Logger;
+
 import com.tatol.service.api.CalculatriceService;
 import com.tatol.vo.OperationIn;
 import com.tatol.vo.OperationOut;
 
 public class CalculatriceServiceImpl implements CalculatriceService {
 
+	private static Logger logger = Logger.getLogger(CalculatriceServiceImpl.class);
+
 	public OperationOut addition(OperationIn in) {
 		OperationOut result = null;
-		if(in != null){
+		if (in != null) {
 			int resultat = 0;
 			result = new OperationOut();
-			if(in.getOperator() != null){
-				switch (in.getOperator()){
-					case "+": resultat = in.getFirstNumber() + in.getSecondNumber();
-					break;
-					case "-": resultat = in.getFirstNumber() - in.getSecondNumber();
-					break;
-					case "*": resultat = in.getFirstNumber() * in.getSecondNumber();
-					break;
-					case "/": resultat = in.getFirstNumber() / in.getSecondNumber();
-					break;
-					default: resultat = in.getFirstNumber() + in.getSecondNumber();
-					break;
+			if (in.getOperator() != null) {
+				try {
+					switch (in.getOperator()) {
+					case "+":
+						resultat = in.getFirstNumber() + in.getSecondNumber();
+						break;
+					case "-":
+						resultat = in.getFirstNumber() - in.getSecondNumber();
+						break;
+					case "*":
+						resultat = in.getFirstNumber() * in.getSecondNumber();
+						break;
+					case "/":
+						resultat = in.getFirstNumber() / in.getSecondNumber();
+						break;
+					default:
+						resultat = in.getFirstNumber() + in.getSecondNumber();
+						break;
+					}
+				} catch (Exception e) {
+					logger.error("Error while processin addition with operator" + in.getOperator(), e);
 				}
 			} else {
 				resultat = in.getFirstNumber() + in.getSecondNumber();
